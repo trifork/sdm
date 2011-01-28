5b1ead93dd266e0855de03e6b6e5420682dcfb4e
Thomas Ardal <tar@trifork.com>
2011-01-28 14:29:53 +0100

    small changes

--- subprojects/replication/src/test/java/com/trifork/sdm/replication/replication/XMLEntityWriterTest.java
+++ subprojects/replication/src/test/java/com/trifork/sdm/replication/replication/XMLEntityWriterTest.java
@@ -4,6 +4,7 @@ import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.*;
 
 import java.sql.*;
+
 import java.util.Calendar;
 
 import javax.inject.Provider;
@@ -13,6 +14,7 @@ import org.junit.Test;
 import com.google.inject.Provides;
 import com.trifork.sdm.replication.GuiceTest;
 import com.trifork.sdm.replication.util.URLFactory;
+import com.trifork.stamdata.registre.sor.Apotek;
 
 
 public class XMLEntityWriterTest extends GuiceTest
@@ -33,29 +35,26 @@ public class XMLEntityWriterTest extends GuiceTest
 		when(resultSet.getTimestamp("ModifiedDate")).thenReturn(new Timestamp(now.getTimeInMillis()));
 		when(resultSet.getTimestamp("ValidFrom")).thenReturn(new Timestamp(Integer.MIN_VALUE));
 		when(resultSet.getTimestamp("ValidTo")).thenReturn(new Timestamp(Integer.MAX_VALUE));
-
+		
 		PreparedStatement preparedStatement = mock(PreparedStatement.class);
 		when(preparedStatement.executeQuery()).thenReturn(resultSet);
-
+		
 		Connection connection = mock(Connection.class);
 		when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
 
-		Provider<Connection> provider = mock(Provider.class);
+		Provider<Connection> provider = (Provider<Connection>) mock(Provider.class);
 		when(provider.get()).thenReturn(connection);
 
 		now.add(Calendar.DAY_OF_MONTH, -1);
-
-		// URLFactory urlFactory = mock(URLFactory.class);
-		// when(urlFactory.create(any(Class.class), anyInt(), anyString(),
-		// anyString())).
-
-		// XMLEntityWriter xmlEntityWriter = new XMLEntityWriter(provider,
-		// urlFactory);
-		// ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		//
-		// // Act
-		// xmlEntityWriter.write(outputStream, Apotek.class, OutputFormat.XML,
-		// 100, now.getTime(), 0);
+		
+//		URLFactory urlFactory = mock(URLFactory.class);
+//		when(urlFactory.create(any(Class.class), anyInt(), anyString(), anyString())).
+		
+//		XMLEntityWriter xmlEntityWriter = new XMLEntityWriter(provider, urlFactory);
+//		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+//		
+//		// Act
+//		xmlEntityWriter.write(outputStream, Apotek.class, OutputFormat.XML, 100, now.getTime(), 0);
 
 		// Assert
 	}
