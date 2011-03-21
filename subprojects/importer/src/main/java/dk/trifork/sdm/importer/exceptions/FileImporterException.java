bdc72648bc91c4ddd15dc7e41ddb1258dc09fbcf
Thomas Børlum <thb@trifork.com>
2011-03-22 00:28:35 +0100

    Formatted and added serial versions to exceptions.

--- subprojects/importer/src/main/java/dk/trifork/sdm/importer/exceptions/FileImporterException.java
+++ subprojects/importer/src/main/java/dk/trifork/sdm/importer/exceptions/FileImporterException.java
@@ -1,12 +1,16 @@
 package dk.trifork.sdm.importer.exceptions;
 
 public class FileImporterException extends Exception {
-	
-	public FileImporterException(String message, Throwable cause) {
-		super(message, cause);
-	}
-	
+
+	private static final long serialVersionUID = -1565028705535915764L;
+
 	public FileImporterException(String message) {
+
 		super(message);
+	}
+
+	public FileImporterException(String message, Throwable cause) {
+
+		super(message, cause);
 	}
 }
--- subprojects/importer/src/main/java/dk/trifork/sdm/importer/exceptions/FileParseException.java
+++ subprojects/importer/src/main/java/dk/trifork/sdm/importer/exceptions/FileParseException.java
@@ -2,13 +2,15 @@ package dk.trifork.sdm.importer.exceptions;
 
 public class FileParseException extends FileImporterException {
 
+	private static final long serialVersionUID = -4956915985637502728L;
 
+	public FileParseException(String message) {
 
-	public FileParseException(String message, Throwable cause) {
-		super(message, cause);
+		super(message);
 	}
 
-    public FileParseException(String message) {
-        super(message);
-    }
+	public FileParseException(String message, Throwable cause) {
+
+		super(message, cause);
+	}
 }
