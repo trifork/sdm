edf52e4201b52c16f3f413d7ffe366983c5479b1
Thomas Børlum <thb@trifork.com>
2011-03-22 11:37:01 +0100

    Removed Guice dependency from the shared lib.

--- subprojects/shared/src/main/java/com/trifork/stamdata/Nullable.java
+++ subprojects/shared/src/main/java/com/trifork/stamdata/Nullable.java
@@ -4,13 +4,12 @@ import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
-import com.google.inject.BindingAnnotation;
 
 @Retention(RUNTIME)
 @Target({ FIELD, PARAMETER, METHOD })
-@BindingAnnotation
 public @interface Nullable {
 
 }
