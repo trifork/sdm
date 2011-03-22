f0d0041312626dee656c25f64a21f60a6810b149
Thomas Børlum <thb@trifork.com>
2011-03-22 11:39:39 +0100

    Fixed error that converted '000' record ids.
    The JPA BigInteger adapter could not handle some
    strings like '0000000'. This should be moved
    somewhere else at some point.

--- subprojects/replication/src/main/java/com/trifork/stamdata/replication/replication/RecordDao.java
+++ subprojects/replication/src/main/java/com/trifork/stamdata/replication/replication/RecordDao.java
@@ -1,5 +1,6 @@
 package com.trifork.stamdata.replication.replication;
 
+import java.math.BigInteger;
 import java.util.Date;
 import java.util.List;
 import javax.persistence.*;
@@ -20,13 +21,17 @@ public class RecordDao {
 
 	public <T extends View> List<T> findPage(Class<T> type, String recordId, Date modifiedDate, int limit) {
 
+		// TODO: This convertion should not take place here.
+		
+		BigInteger id = new BigInteger(recordId);
+		
 		// CREATE A QUERY
 		//
 
 		String SQL = "FROM " + type.getName() + " WHERE (recordID = :recordID AND modifiedDate > :modifiedDate) OR (recordID > :recordID) ORDER BY recordID, modifiedDate";
 
 		TypedQuery<T> query = em.createQuery(SQL, type);
-		query.setParameter("recordID", recordId);
+		query.setParameter("recordID", id);
 		query.setParameter("modifiedDate", modifiedDate);
 		query.setMaxResults(limit);
 		query.setHint(QueryHints.HINT_READONLY, true);
