4fc570093258c799ca1567c38018f49d922f080a
Thomas Børlum <thb@trifork.com>
2011-03-22 11:37:37 +0100

    The @Documented annotation retained at runtime.

--- subprojects/shared/src/main/java/com/trifork/stamdata/Documented.java
+++ subprojects/shared/src/main/java/com/trifork/stamdata/Documented.java
@@ -1,5 +1,10 @@
 package com.trifork.stamdata;
 
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+
+@Retention(RUNTIME)
 public @interface Documented {
 	String value();
 }
