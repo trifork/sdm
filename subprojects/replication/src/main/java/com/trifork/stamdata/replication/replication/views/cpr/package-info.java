0d4fb90a37be5303d0ac33df965e868468934c84
Thomas Børlum <thb@trifork.com>
2011-03-24 11:46:24 +0100

    Made namespaces per package with annotations.

---
+++ subprojects/replication/src/main/java/com/trifork/stamdata/replication/replication/views/cpr/package-info.java
@@ -0,0 +1,5 @@
+@XmlSchema(namespace = STAMDATA_3_0 + "/cpr", elementFormDefault = XmlNsForm.UNQUALIFIED)
+package com.trifork.stamdata.replication.replication.views.cpr;
+
+import javax.xml.bind.annotation.*;
+import static com.trifork.stamdata.replication.util.Namespace.*;
