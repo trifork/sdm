4fdeb03c060452d9e38c64e9ce0d8e3f9b3bee3a
Anders Hessellund Jensen <ahj@trifork.com>
2011-04-08 11:44:40 +0200

    Adding missing sql statements to schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1047,6 +1047,37 @@ CREATE TABLE Udrejseoplysninger (
 	CONSTRAINT Udrejse_Person_1 UNIQUE (CPR, ValidFrom)
 ) ENGINE=InnoDB COLLATE=utf8_danish_ci;
 
+CREATE TABLE Foedselsregistreringsoplysninger(
+	FoedselsregistreringsoplysningerPID BIGINT(15) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	CPR VARCHAR(10) NOT NULL,
+	Foedselsregistreringsstedkode VARCHAR(4) NOT NULL,
+	foedselsregistreringstekst VARCHAR(20) NOT NULL,
+	ModifiedBy VARCHAR(200) NOT NULL,
+	ModifiedDate DATETIME NOT NULL,
+	ValidFrom DATETIME NOT NULL,
+	ValidTo DATETIME,
+	CreatedBy VARCHAR(200) NOT NULL,
+	CreatedDate DATETIME NOT NULL,
+	INDEX (ValidFrom, ValidTo),
+	CONSTRAINT Foedsel_Person_1 UNIQUE (CPR, ValidFrom)
+) ENGINE=InnoDB COLLATE=utf8_danish_ci;
+
+CREATE TABLE Statsborgerskab(
+	StatsborgerskabPID BIGINT(15) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	CPR VARCHAR(10) NOT NULL,
+	landekode VARCHAR(4) NOT NULL,
+	Statsborgerskabstartdato DATETIME NOT NULL,
+	StatsborgerskabstartdatoUsikkerhedsmarkering VARCHAR(1) NOT NULL,
+	ModifiedBy VARCHAR(200) NOT NULL,
+	ModifiedDate DATETIME NOT NULL,
+	ValidFrom DATETIME NOT NULL,
+	ValidTo DATETIME,
+	CreatedBy VARCHAR(200) NOT NULL,
+	CreatedDate DATETIME NOT NULL,
+	INDEX (ValidFrom, ValidTo),
+	CONSTRAINT Statsborgerskab_Person_1 UNIQUE (CPR, ValidFrom)
+) ENGINE=InnoDB COLLATE=utf8_danish_ci;
+
 CREATE TABLE Valgoplysninger (
 	ValgoplysningerPID BIGINT(15) AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	CPR VARCHAR(10) NOT NULL,
