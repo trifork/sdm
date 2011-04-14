4191647ba784a4ff4c9ea0ac33c4d97ccc5b1b3a
Anders Hessellund Jensen <ahj@trifork.com>
2011-04-14 10:21:17 +0200

    Add statsborgerskab view.

---
+++ subprojects/replication/src/main/java/com/trifork/stamdata/replication/replication/views/cpr/Statsborgerskab.java
@@ -0,0 +1,62 @@
+package com.trifork.stamdata.replication.replication.views.cpr;
+
+import static javax.persistence.TemporalType.TIMESTAMP;
+
+import java.math.BigInteger;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+import com.trifork.stamdata.replication.replication.annotations.ViewPath;
+import com.trifork.stamdata.replication.replication.views.View;
+
+
+@Entity
+@XmlRootElement
+@ViewPath("cpr/statsborgerskab/v1")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Statsborgerskab extends View {
+
+	@Id
+	@GeneratedValue
+	@XmlTransient
+	@Column(name = "StatsborgerskabPID")
+	protected BigInteger recordID;
+	protected String cpr;
+	protected String landekode;
+	protected Date statsborgerskabstartdato;
+	protected String statsborgerskabstartdatoUsikkerhedsmarkering;
+	
+	@XmlTransient
+	@Temporal(TIMESTAMP)
+	protected Date modifiedDate;
+
+	@Temporal(TIMESTAMP)
+	protected Date validFrom;
+
+	@Temporal(TIMESTAMP)
+	protected Date validTo;
+
+	@Override
+	public String getId() {
+		return cpr;
+	}
+
+	@Override
+	public BigInteger getRecordID() {
+		return recordID;
+	}
+
+	@Override
+	public Date getUpdated() {
+		return modifiedDate;
+	}
+}
