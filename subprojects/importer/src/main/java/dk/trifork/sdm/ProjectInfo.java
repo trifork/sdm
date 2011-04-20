6096587fcd8745ca18bdf1f52a3af8a47def0f7d
Thomas Børlum <thb@trifork.com>
2011-04-20 09:15:32 +0200

    Fixed MANIFEST.MF parsing in importer.

--- subprojects/importer/src/main/java/dk/trifork/sdm/ProjectInfo.java
+++ subprojects/importer/src/main/java/dk/trifork/sdm/ProjectInfo.java
@@ -1,67 +1,91 @@
 package dk.trifork.sdm;
 
-import dk.trifork.sdm.config.Configuration;
+import static org.slf4j.LoggerFactory.getLogger;
 
-import javax.servlet.ServletContext;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
+import javax.servlet.ServletContext;
+
+import org.slf4j.Logger;
+
 
 public class ProjectInfo {
-	private String deployType = "Unknown";
-	private String buildInfo = "Unknown";
-	private String vendor = "Unknown";
-	private String title = "Unknown";
-	private String version = "Unknown";
-	
-	public ProjectInfo(ServletContext sctx) {
-		try {
-			deployType = "WAR";
-			InputStream is = sctx.getResourceAsStream("META-INF/MANIFEST.MF");
-			BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-			String line = reader.readLine();
-			while (line != null){
-				if (line.startsWith("Built-Date:")) {
-					buildInfo = line.substring("Built-Date:".length() + 1);
-				}
-				if (line.startsWith("Implementation-Vendor:")) {
-					vendor = line.substring("Implementation-Vendor:".length() + 1);
-				}
-				if (line.startsWith("Implementation-Title:")) {
-					title = line.substring("Implementation-Title:".length() + 1);
-				}
-				if (line.startsWith("Built-Version:")) {
-					version = line.substring("Built-Version:".length() + 1);
-				}
-				line = reader.readLine();
-			}
-		} catch (Exception e) {
-			// No Manifest. Properly a inplace deployment in development environment
-			deployType = "INPLACE";
-			buildInfo =  (new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")).format(new Date());
-			vendor = "Trifork";
-			title = "sdm";
-			version = Configuration.getString("version could not be extracted from MANIFEST.MF");
-		}
 
+	private static final Logger logger = getLogger(ProjectInfo.class);
+
+	private String deployType;
+	private String buildInfo;
+	private String vendor;
+	private String title;
+	private String version;
+
+	public ProjectInfo(ServletContext context) {
+
+		InputStream inputStream = context.getResourceAsStream("/META-INF/MANIFEST.MF");
+		
+		if (inputStream != null) {
+			try {
+
+				BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+				String line = reader.readLine();
+				while (line != null) {
+					if (line.startsWith("Built-Date:")) {
+						buildInfo = line.substring("Built-Date:".length() + 1);
+					}
+					if (line.startsWith("Implementation-Vendor:")) {
+						vendor = line.substring("Implementation-Vendor:".length() + 1);
+					}
+					if (line.startsWith("Implementation-Title:")) {
+						title = line.substring("Implementation-Title:".length() + 1);
+					}
+					if (line.startsWith("Built-Version:")) {
+						version = line.substring("Built-Version:".length() + 1);
+					}
+					line = reader.readLine();
+				}
+
+				reader.close();
+
+				deployType = "WAR";
+			}
+			catch (Exception e) {
+
+				logger.error("Could not load file: 'META-INF/MANIFEST.MF'.", e);
+			}
+		}
+		else {
+			deployType = "Unknown";
+			buildInfo = "Unknown";
+			vendor = "Unknown";
+			title = "Unknown";
+			version = "Unknown";
+		}
 	}
+
 	public String getDeployType() {
+
 		return deployType;
 	}
+
 	public String getBuildInfo() {
+
 		return buildInfo;
 	}
+
 	public String getVendor() {
+
 		return vendor;
 	}
+
 	public String getTitle() {
+
 		return title;
 	}
+
 	public String getVersion() {
+
 		return version;
 	}
-	
-
 }
