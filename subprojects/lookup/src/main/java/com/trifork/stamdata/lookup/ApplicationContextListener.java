18c802d7e1297e9a0f066fd79f9096d0bbcfab1b
Anders Hessellund Jensen <ahj@trifork.com>
2011-05-30 09:28:56 +0200

    Add missing entity classes to session factory

--- subprojects/lookup/src/main/java/com/trifork/stamdata/lookup/ApplicationContextListener.java
+++ subprojects/lookup/src/main/java/com/trifork/stamdata/lookup/ApplicationContextListener.java
@@ -19,7 +19,17 @@ import com.trifork.stamdata.db.SessionFactoryModule;
 import com.trifork.stamdata.lookup.rest.RestModule;
 import com.trifork.stamdata.lookup.security.SecurityModule;
 import com.trifork.stamdata.ssl.OcesSslModule;
+import com.trifork.stamdata.views.cpr.BarnRelation;
+import com.trifork.stamdata.views.cpr.Civilstand;
+import com.trifork.stamdata.views.cpr.Foedselsregistreringsoplysninger;
+import com.trifork.stamdata.views.cpr.Folkekirkeoplysninger;
+import com.trifork.stamdata.views.cpr.ForaeldremyndighedsRelation;
+import com.trifork.stamdata.views.cpr.KommunaleForhold;
 import com.trifork.stamdata.views.cpr.Person;
+import com.trifork.stamdata.views.cpr.Statsborgerskab;
+import com.trifork.stamdata.views.cpr.Udrejseoplysninger;
+import com.trifork.stamdata.views.cpr.UmyndiggoerelseVaergeRelation;
+import com.trifork.stamdata.views.cpr.Valgoplysninger;
 
 public class ApplicationContextListener extends GuiceServletContextListener {
 
@@ -52,6 +62,16 @@ public class ApplicationContextListener extends GuiceServletContextListener {
 			modules.add(new RestModule());
 			Set<Class<?>> entityClasses = new HashSet<Class<?>>();
 			entityClasses.add(Person.class);
+			entityClasses.add(Folkekirkeoplysninger.class);
+			entityClasses.add(Statsborgerskab.class);
+			entityClasses.add(Foedselsregistreringsoplysninger.class);
+			entityClasses.add(Civilstand.class);
+			entityClasses.add(BarnRelation.class);
+			entityClasses.add(ForaeldremyndighedsRelation.class);
+			entityClasses.add(KommunaleForhold.class);
+			entityClasses.add(Udrejseoplysninger.class);
+			entityClasses.add(UmyndiggoerelseVaergeRelation.class);
+			entityClasses.add(Valgoplysninger.class);
 			modules.add(new SessionFactoryModule (
 				config.getString("db.connection.driverClass"),
 				config.getString("db.connection.sqlDialect"),
