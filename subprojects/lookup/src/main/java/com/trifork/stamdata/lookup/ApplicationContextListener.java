71d86bf24c59cb6c770e31438c55e083af75f3f0
Flemming Jønsson <frj@trifork.com>
2011-06-08 08:15:43 +0200

    MorOgFader entity registered as entityClass so lookup can .... look it up. Javascript client enhanced so it displayes, mother, father and spouse (if any)

--- subprojects/lookup/src/main/java/com/trifork/stamdata/lookup/ApplicationContextListener.java
+++ subprojects/lookup/src/main/java/com/trifork/stamdata/lookup/ApplicationContextListener.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import com.trifork.stamdata.views.cpr.*;
 import org.apache.commons.configuration.Configuration;
 import org.slf4j.Logger;
 
@@ -19,18 +20,6 @@ import com.trifork.stamdata.db.SessionFactoryModule;
 import com.trifork.stamdata.lookup.rest.RestModule;
 import com.trifork.stamdata.lookup.security.SecurityModule;
 import com.trifork.stamdata.ssl.OcesSslModule;
-import com.trifork.stamdata.views.cpr.BarnRelation;
-import com.trifork.stamdata.views.cpr.Beskyttelse;
-import com.trifork.stamdata.views.cpr.Civilstand;
-import com.trifork.stamdata.views.cpr.Foedselsregistreringsoplysninger;
-import com.trifork.stamdata.views.cpr.Folkekirkeoplysninger;
-import com.trifork.stamdata.views.cpr.ForaeldremyndighedsRelation;
-import com.trifork.stamdata.views.cpr.KommunaleForhold;
-import com.trifork.stamdata.views.cpr.Person;
-import com.trifork.stamdata.views.cpr.Statsborgerskab;
-import com.trifork.stamdata.views.cpr.Udrejseoplysninger;
-import com.trifork.stamdata.views.cpr.UmyndiggoerelseVaergeRelation;
-import com.trifork.stamdata.views.cpr.Valgoplysninger;
 
 public class ApplicationContextListener extends GuiceServletContextListener {
 
@@ -62,18 +51,20 @@ public class ApplicationContextListener extends GuiceServletContextListener {
 			
 			modules.add(new RestModule());
 			Set<Class<?>> entityClasses = new HashSet<Class<?>>();
-			entityClasses.add(Person.class);
-			entityClasses.add(Folkekirkeoplysninger.class);
-			entityClasses.add(Statsborgerskab.class);
-			entityClasses.add(Foedselsregistreringsoplysninger.class);
-			entityClasses.add(Civilstand.class);
-			entityClasses.add(BarnRelation.class);
-			entityClasses.add(ForaeldremyndighedsRelation.class);
-			entityClasses.add(KommunaleForhold.class);
-			entityClasses.add(Udrejseoplysninger.class);
-			entityClasses.add(UmyndiggoerelseVaergeRelation.class);
-			entityClasses.add(Valgoplysninger.class);
-			entityClasses.add(Beskyttelse.class);
+            entityClasses.add(BarnRelation.class);
+            entityClasses.add(Beskyttelse.class);
+            entityClasses.add(Civilstand.class);
+            entityClasses.add(Foedselsregistreringsoplysninger.class);
+            entityClasses.add(Folkekirkeoplysninger.class);
+            entityClasses.add(ForaeldremyndighedsRelation.class);
+            entityClasses.add(KommunaleForhold.class);
+            entityClasses.add(MorOgFaroplysninger.class);
+            entityClasses.add(Person.class);
+            entityClasses.add(Statsborgerskab.class);
+            entityClasses.add(Udrejseoplysninger.class);
+            entityClasses.add(UmyndiggoerelseVaergeRelation.class);
+            entityClasses.add(Valgoplysninger.class);
+
 			modules.add(new SessionFactoryModule (
 				config.getString("db.connection.driverClass"),
 				config.getString("db.connection.sqlDialect"),
