e5dd706baf1715e216980df7e43784bd799c777f
Flemming Jønsson <frj@trifork.com>
2011-06-14 13:09:52 +0200

    Review and refactoring of javascript lookup client

--- subprojects/lookup/src/main/java/com/trifork/stamdata/lookup/rest/PersonClientResource.java
+++ subprojects/lookup/src/main/java/com/trifork/stamdata/lookup/rest/PersonClientResource.java
@@ -14,14 +14,24 @@ import java.util.Scanner;
 
 @Path("personclient")
 public class PersonClientResource {
+    private static final String FILE_START_TOKEN = "\\A";
     @Context
     private ServletContext servletContext;
 
     @GET
     @Produces("text/html")
     public String renderPerson(@Context HttpServletRequest request) {
-        InputStream is = servletContext.getResourceAsStream("/client/jquery.html");
-        String javascriptClient = new Scanner(is).useDelimiter("\\A").next();
+        InputStream is = servletContext.getResourceAsStream("/client/client.html");
+        String javascriptClient = convertInputStreamToString(is);
+
+        return substitutePlaceholders(request, javascriptClient);
+    }
+
+    private String substitutePlaceholders(HttpServletRequest request, String javascriptClient) {
         return StringUtils.replace(javascriptClient, "${context.path}", request.getContextPath());
+    }
+
+    private String convertInputStreamToString(InputStream is) {
+        return new Scanner(is).useDelimiter(FILE_START_TOKEN).next();
     }
 }
