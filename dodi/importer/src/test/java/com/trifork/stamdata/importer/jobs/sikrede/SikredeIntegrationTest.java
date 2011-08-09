cca116b26882958deab5fca5c176ff8d296067f6
Flemming Jønsson <frj@trifork.com>
2011-08-09 15:51:36 +0200

    Test af de enkelte felter der oprettes efter import af 20110701.SIKREDE

--- dodi/importer/src/test/java/com/trifork/stamdata/importer/jobs/sikrede/SikredeIntegrationTest.java
+++ dodi/importer/src/test/java/com/trifork/stamdata/importer/jobs/sikrede/SikredeIntegrationTest.java
@@ -11,9 +11,12 @@ import org.junit.Test;
 import java.io.File;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 
 public class SikredeIntegrationTest
@@ -44,13 +47,70 @@ public class SikredeIntegrationTest
 		importFile("data/sikrede/20110701.SIKREDE");
 
 		Statement stmt = con.createStatement();
-		ResultSet rs = stmt.executeQuery("SELECT ValidFrom FROM Sikrede");
-		rs.next();
-		assertEquals(new DateTime(2011, 7, 13, 0, 0, 0, 0), new DateTime(rs.getTimestamp(1)));
+		ResultSet rs = stmt.executeQuery("SELECT * FROM Sikrede WHERE CPR='1607769871'");
+		assertTrue("No 'Sikrede' row found for CPR 1607769871", rs.next());
+		assertEquals("ValidFrom matcher ikke",new DateTime(2011, 7, 13, 0, 0, 0, 0), new DateTime(rs.getTimestamp("ValidFrom")));
+        assertEquals("foelgeskabsPersonCpr matcher ikke","2108751234", rs.getString("foelgeskabsPersonCpr"));
+        assertEquals("kommunekode matcher ikke","851", rs.getString("kommunekode"));
+        assertEquals("kommunekodeIkraftDato matcher ikke", new DateTime(2001,2, 1, 0, 0, 0, 0), new DateTime(rs.getTimestamp("kommunekodeIkraftDato")));
+        assertNull("Status", rs.getString("status"));
+        assertEquals("bevisIkraftDato matcher ikke", new DateTime(2011, 2, 1, 0, 0, 0, 0), new DateTime(rs.getTimestamp("bevisIkraftDato")));
+        assertEquals("forsikringsinstans matcher ikke", "Sygesikring", rs.getString("forsikringsinstans"));
+        assertEquals("forsikringsinstansKode matcher ikke", "0123456789", rs.getString("forsikringsinstansKode"));
+        assertEquals("forsikringsnummer matcher ikke", "010101010101010", rs.getString("forsikringsnummer"));
+        assertEquals("sslGyldigFra matcher ikke", new DateTime(2000, 1, 1, 0,0,0,0), new DateTime(rs.getTimestamp("sslGyldigFra")));
+        assertEquals("sslGyldigTil matcher ikke", new DateTime(9999,12,31, 0,0,0,0), new DateTime(rs.getTimestamp("sslGyldigTil")));
+        assertEquals("socialLand matcher ikke", "Danmark", rs.getString("socialLand"));
+        assertEquals("socialLandKode matcher ikke", "DK", rs.getString("socialLandKode"));
+        rs.close();
+
+        rs = stmt.executeQuery("SELECT * FROM SikredeYderRelation WHERE CPR='1607769871' AND type='C'");
+        int ydernummer = 4294;
+        DateTime ydernummerIkraftDato = new DateTime(2009, 1, 1, 0, 0, 0, 0);
+        DateTime gruppekodeRegistreringDato = new DateTime(2000, 12, 1, 0, 0, 0, 0);
+        DateTime ydernummerRegistreringsDato = new DateTime(2008, 12, 1, 0, 0, 0, 0);
+        String sikringsgruppekode = "1";
+        DateTime gruppeKodeIkraftDato = new DateTime(2001, 1, 1, 0, 0, 0, 0);
+        assertSikredeYderRelation(rs, ydernummer, ydernummerIkraftDato, ydernummerRegistreringsDato, sikringsgruppekode, gruppeKodeIkraftDato, gruppekodeRegistreringDato);
+        rs.close();
+
+
+        rs = stmt.executeQuery("SELECT * FROM SikredeYderRelation WHERE CPR='1607769871' AND type='F'");
+        ydernummer = 4296;
+        ydernummerIkraftDato = new DateTime(2007,1,1,0,0,0,0);
+        ydernummerRegistreringsDato = new DateTime(2006,12,1,0,0,0,0);
+        sikringsgruppekode = "1";
+        gruppeKodeIkraftDato = new DateTime(2001,1,1,0,0,0,0);
+        gruppekodeRegistreringDato = new DateTime(2000,12,1,0,0,0,0);
+        assertSikredeYderRelation(rs, ydernummer, ydernummerIkraftDato, ydernummerRegistreringsDato, sikringsgruppekode, gruppeKodeIkraftDato, gruppekodeRegistreringDato);
+        rs.close();
+
+        rs = stmt.executeQuery("SELECT * FROM SikredeYderRelation WHERE CPR='1607769871' AND type='P'");
+        ydernummer = 4295;
+        ydernummerIkraftDato = new DateTime(2008, 1, 1, 0, 0, 0, 0);
+        ydernummerRegistreringsDato = new DateTime(2007,12,1,0,0,0,0);
+        sikringsgruppekode = "1";
+        gruppeKodeIkraftDato = new DateTime(2001,1,1,0,0,0,0);
+        gruppekodeRegistreringDato = new DateTime(2000,12,1,0,0,0,0);
+        assertSikredeYderRelation(rs, ydernummer, ydernummerIkraftDato, ydernummerRegistreringsDato, sikringsgruppekode, gruppeKodeIkraftDato, gruppekodeRegistreringDato);
+        rs.close();
+
+        stmt.close();
 	}
 
-	@Test
-	public void canImportPersonWithSaerligSunhedskort() throws Exception
+    private void assertSikredeYderRelation(ResultSet rs, int ydernummer, DateTime ydernummerIkraftDato, DateTime ydernummerRegistreringDato, String sikringsgruppeKode, DateTime gruppeKodeIkraftDato, DateTime gruppekodeRegistreringDato) throws SQLException {
+        assertTrue("No current 'SikredeYderRelation' row found", rs.next());
+        String type = rs.getString("type");
+        assertEquals("SikredeYderRelation type '"+type+"' ydernummer matcher ikke", ydernummer, rs.getInt("ydernummer"));
+        assertEquals("SikredeYderRelation type '"+type+"' ydernummerIkraftDato matcher ikke", ydernummerIkraftDato, new DateTime(rs.getTimestamp("ydernummerIkraftDato")));
+        assertEquals("SikredeYderRelation type '"+type+"' ydernummerRegistreringDato matcher ikke", ydernummerRegistreringDato, new DateTime(rs.getTimestamp("ydernummerRegistreringDato")));
+        assertEquals("SikredeYderRelation type '"+type+"' sikringsgruppeKode matcher ikke", sikringsgruppeKode, rs.getString("sikringsgruppeKode"));
+        assertEquals("SikredeYderRelation type '"+type+"' gruppeKodeIkraftDato matcher ikke", gruppeKodeIkraftDato, new DateTime(rs.getTimestamp("gruppeKodeIkraftDato")));
+        assertEquals("SikredeYderRelation type '" + type + "' gruppekodeRegistreringDato matcher ikke", gruppekodeRegistreringDato, new DateTime(rs.getTimestamp("gruppekodeRegistreringDato")));
+    }
+
+    @Test
+	public void canImportPersonWithSaerligSundhedskort() throws Exception
 	{
 		importFile("data/sikrede/20110701.SIKREDE_SSK");
 
@@ -92,5 +152,6 @@ public class SikredeIntegrationTest
 		SikredeParser parser = new SikredeParser();
 		File file = FileUtils.toFile(getClass().getClassLoader().getResource(fileName));
 		parser.importFiles(new File[] { file }, new AuditingPersister(con));
+        //con.commit();
 	}
 }
