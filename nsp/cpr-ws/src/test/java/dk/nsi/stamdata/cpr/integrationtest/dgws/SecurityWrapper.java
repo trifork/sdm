9c951677ce2676e84afb8ec5fa59b823a4d5dd50
Thomas Børlum <thb@trifork.com>
2011-09-13 13:28:37 +0200

    Add ID Card Builder.

---
+++ nsp/cpr-ws/src/test/java/dk/nsi/stamdata/cpr/integrationtest/dgws/SecurityWrapper.java
@@ -0,0 +1,50 @@
+/**
+ * The MIT License
+ *
+ * Original work sponsored and donated by National Board of e-Health (NSI), Denmark
+ * (http://www.nsi.dk)
+ *
+ * Copyright (C) 2011 National Board of e-Health (NSI), Denmark (http://www.nsi.dk)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package dk.nsi.stamdata.cpr.integrationtest.dgws;
+
+import org.oasis_open.docs.wss._2004._01.oasis_200401_wss_wssecurity_secext_1_0.Security;
+
+import dk.medcom.dgws._2006._04.dgws_1_0.Header;
+
+public class SecurityWrapper {
+
+	private Security security = null;
+	private Header medcomHeader = null;
+
+	public SecurityWrapper(Security security, Header medcomHeader) {
+		this.security = security;
+		this.medcomHeader = medcomHeader;
+	}
+
+	public Security getSecurity() {
+		return security;
+	}
+
+	public Header getMedcomHeader() {
+		return medcomHeader;
+	}
+}
