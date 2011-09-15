94edb5af1a5f205a43cc1033c781bb68487eb8ca
Thomas Børlum <thb@trifork.com>
2011-09-15 18:44:40 +0200

    Add initial Sikrede entity to common lib.

---
+++ common/common/src/main/java/com/trifork/stamdata/models/sikrede/Sikrede.java
@@ -0,0 +1,9 @@
+package com.trifork.stamdata.models.sikrede;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Sikrede
+{
+	// TODO: Implement this or move the implementation from the data manager and make it work with the CPR service.
+}
