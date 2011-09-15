1ea2add37fe226680e13aea55c97289b63f5083f
Thomas Børlum <thb@trifork.com>
2011-09-15 18:45:33 +0200

    Add person with healthcare mapper to CPR service.

---
+++ nsp/cpr-ws/src/main/java/dk/nsi/stamdata/cpr/PersonWithHealthCareMapper.java
@@ -0,0 +1,36 @@
+package dk.nsi.stamdata.cpr;
+
+import static com.trifork.stamdata.Preconditions.checkNotNull;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+
+import com.google.inject.Inject;
+import com.trifork.stamdata.models.cpr.Person;
+import com.trifork.stamdata.models.sikrede.Sikrede;
+
+import dk.nsi.stamdata.cpr.ws.PersonInformationStructureType;
+import dk.nsi.stamdata.cpr.ws.PersonWithHealthCareInformationStructureType;
+
+public class PersonWithHealthCareMapper
+{
+	private PersonMapper personMapper;
+
+	@Inject
+	PersonWithHealthCareMapper(PersonMapper personMapper)
+	{
+		this.personMapper = checkNotNull(personMapper, "personMapper");
+	}
+
+	public PersonWithHealthCareInformationStructureType map(Person person, Sikrede sikrede) throws DatatypeConfigurationException
+	{
+		PersonWithHealthCareInformationStructureType personWithHealthCare = new PersonWithHealthCareInformationStructureType();
+
+		PersonInformationStructureType personInformation = personMapper.map(person);
+
+		personWithHealthCare.setPersonInformationStructure(personInformation);
+
+		// TODO: Map the person information.
+
+		return null;
+	}
+}
