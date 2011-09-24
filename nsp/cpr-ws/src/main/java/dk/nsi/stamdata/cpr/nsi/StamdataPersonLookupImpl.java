8ef838d6e52443b0c82f16b1c599ff069c70b961
Thomas Børlum <thb@trifork.com>
2011-09-24 20:26:36 +0200

    Add WSDL and skeleton for getPersonDetails ws.

---
+++ nsp/cpr-ws/src/main/java/dk/nsi/stamdata/cpr/nsi/StamdataPersonLookupImpl.java
@@ -0,0 +1,19 @@
+package dk.nsi.stamdata.cpr.nsi;
+
+import javax.xml.ws.Holder;
+
+import dk.nsi.stamdata.cpr.ws.DGWSFault;
+import dk.nsi.stamdata.cpr.ws.Header;
+import dk.nsi.stamdata.cpr.ws.PersonLookupRequestType;
+import dk.nsi.stamdata.cpr.ws.PersonLookupResponseType;
+import dk.nsi.stamdata.cpr.ws.Security;
+import dk.nsi.stamdata.cpr.ws.StamdataPersonLookup;
+
+public class StamdataPersonLookupImpl implements StamdataPersonLookup
+{
+	@Override
+	public PersonLookupResponseType getPersonDetails(Holder<Security> wsseHeader, Holder<Header> medcomHeader, PersonLookupRequestType parameters) throws DGWSFault
+	{
+		return null;
+	}
+}
