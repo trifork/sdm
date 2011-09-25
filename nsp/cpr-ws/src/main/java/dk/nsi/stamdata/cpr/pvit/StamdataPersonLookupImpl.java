3fa00300641c0e900ccb8dc7c7dd91ed7fb5ec9b
Thomas Børlum <thb@trifork.com>
2011-09-25 12:54:59 +0200

    Start implementing P-VIT.

--- nsp/cpr-ws/src/main/java/dk/nsi/stamdata/cpr/pvit/StamdataPersonLookupImpl.java
+++ nsp/cpr-ws/src/main/java/dk/nsi/stamdata/cpr/pvit/StamdataPersonLookupImpl.java
@@ -1,18 +1,34 @@
 package dk.nsi.stamdata.cpr.pvit;
 
+import java.util.Set;
+
 import javax.jws.WebService;
 import javax.xml.ws.Holder;
 
+import com.google.inject.Inject;
+
+import dk.nsi.stamdata.cpr.annotations.Whitelist;
 import dk.nsi.stamdata.cpr.ws.DGWSFault;
 import dk.nsi.stamdata.cpr.ws.Header;
 import dk.nsi.stamdata.cpr.ws.PersonLookupRequestType;
 import dk.nsi.stamdata.cpr.ws.PersonLookupResponseType;
 import dk.nsi.stamdata.cpr.ws.Security;
 import dk.nsi.stamdata.cpr.ws.StamdataPersonLookup;
+import dk.sosi.seal.model.SystemIDCard;
 
 @WebService(serviceName = "StamdataPersonLookup", endpointInterface = "dk.nsi.stamdata.cpr.ws.StamdataPersonLookup")
 public class StamdataPersonLookupImpl implements StamdataPersonLookup
 {
+	private final Set<String> whitelist;
+	private final SystemIDCard idCard;
+
+	@Inject
+	StamdataPersonLookupImpl(@Whitelist Set<String> whitelist, SystemIDCard idCard)
+	{
+		this.whitelist = whitelist;
+		this.idCard = idCard;
+	}
+	
 	@Override
 	public PersonLookupResponseType getPersonDetails(Holder<Security> wsseHeader, Holder<Header> medcomHeader, PersonLookupRequestType parameters) throws DGWSFault
 	{
