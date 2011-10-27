43de3e2154e9b80bd3cd9d63503773ee6e6c5d35
Thomas Børlum <thb@trifork.com>
2011-10-27 09:15:48 +0200

    Fix logging in data manager with config file.

---
+++ dodi/data-manager/src/main/resources/logback.groovy
@@ -0,0 +1,25 @@
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.core.ConsoleAppender
+import ch.qos.logback.core.status.OnConsoleStatusListener
+import ch.qos.logback.core.rolling.RollingFileAppender
+import static ch.qos.logback.classic.Level.INFO
+import static ch.qos.logback.core.spi.FilterReply.ACCEPT
+import static ch.qos.logback.core.spi.FilterReply.DENY
+import ch.qos.logback.core.rolling.TimeBasedRollingPolicy
+
+def LOGS_DIR = System.getProperty("jboss.server.log.dir")
+def LOG_FILE = "stamdata-data-manager-ws.log"
+
+appender("ROLLING", RollingFileAppender) {
+    file = "${LOGS_DIR}/${LOG_FILE}"
+
+    encoder(PatternLayoutEncoder) {
+        pattern = "%d [%-4level] %msg [%thread] [%mdc]%n"
+    }
+
+    rollingPolicy(TimeBasedRollingPolicy) {
+        FileNamePattern = "${LOGS_DIR}/${LOG_FILE}.%d{yyyy-MM}.zip"
+    }
+}
+
+root(INFO, ['ROLLING'])
