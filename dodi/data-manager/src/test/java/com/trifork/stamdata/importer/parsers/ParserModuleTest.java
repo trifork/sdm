2f65cdca203a71de6de69a528e51c5d3a25ebf8c
Thomas Børlum <thb@trifork.com>
2011-11-20 10:57:32 +0100

    Remove unused regex test matcher.

---
+++ dodi/data-manager/src/test/java/com/trifork/stamdata/importer/parsers/ParserModuleTest.java
@@ -0,0 +1,22 @@
+package com.trifork.stamdata.importer.parsers;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ParserModuleTest
+{
+    private ParserScope scope;
+
+    @Before
+    public void setUp()
+    {
+
+    }
+    
+    @Test
+    public void testThatTransactionTimeIsBoundToASingleInstanceInParserScope()
+    {
+        
+    }
+}
