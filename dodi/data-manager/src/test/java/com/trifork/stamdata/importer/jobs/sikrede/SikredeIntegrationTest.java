cab0a88df737c922aa92fefaa36a1f877772870d
Thomas Børlum <thb@trifork.com>
2011-11-21 14:25:41 +0100

    Fix sikrede integration test.

--- dodi/data-manager/src/test/java/com/trifork/stamdata/importer/jobs/sikrede/SikredeIntegrationTest.java
+++ dodi/data-manager/src/test/java/com/trifork/stamdata/importer/jobs/sikrede/SikredeIntegrationTest.java
@@ -1,12 +1,31 @@
 package com.trifork.stamdata.importer.jobs.sikrede;
 
-/**
- * Created by IntelliJ IDEA.
- * User: borlum
- * Date: 11/21/11
- * Time: 1:49 PM
- * To change this template use File | Settings | File Templates.
- */
-public class SikredeIntegrationTest
+import com.trifork.stamdata.importer.FileParserIntegrationTest;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@Ignore("Ignored until we get some propper test data.")
+public class SikredeIntegrationTest extends FileParserIntegrationTest
 {
+    public SikredeIntegrationTest()
+    {
+        super("sikrede");
+    }
+
+    @Test
+    public void testCanImportAFileSetPlacedInTheInbox() throws Exception
+    {
+        File fileSet = getDirectory("data/sikrede/1");
+
+        placeInInbox(fileSet, true);
+
+        assertThat(isLocked(), is(false));
+
+        assertRecordCount("Sikrede", 1);
+    }
 }
