4efa0f9288239c139840761c5f33b588704a17f7
Flemming Jønsson <frj@trifork.com>
2012-01-18 01:36:26 +0100

    Make CprAbbsStubJettyServer try the specified port twice before failing

--- nsp/cpr-ws/src/test/java/dk/nsi/stamdata/cpr/pvit/proxy/CprAbbsStubJettyServer.java
+++ nsp/cpr-ws/src/test/java/dk/nsi/stamdata/cpr/pvit/proxy/CprAbbsStubJettyServer.java
@@ -27,25 +27,106 @@ package dk.nsi.stamdata.cpr.pvit.proxy;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.webapp.WebAppContext;
 
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.ServerSocket;
+
 public class CprAbbsStubJettyServer {
 
-	private Server server;
+    private Server server;
 
-	public void startServer(int port) throws Exception {
-		server = new Server(port);
+    public void startServer(int desiredPort) throws Exception {
+        if (!available(desiredPort)) {
 
-		WebAppContext cprabbsContext = new WebAppContext();
-		cprabbsContext.setWar("src/test/resources/cprabbs-stub-webapp");
-		cprabbsContext.setContextPath("/cprabbs");
-		server.addHandler(cprabbsContext);
+            //wait n seconds and try again
+            if (!waitSecondsForPort(3000, desiredPort)) {
 
-		server.start();
-	}
+                if (!waitSecondsForPort(1000, desiredPort)) {
+                    throw new RuntimeException("Port " + desiredPort + " is not available - Jetty server cannot be started");
+                }
+            }
 
-	public void stopServer() throws Exception {
-		if (server != null && server.isRunning()) {
-			server.stop();
-		}
-		server = null;
-	}
+        }
+
+        server = new Server(desiredPort);
+
+        WebAppContext cprabbsContext = new WebAppContext();
+        cprabbsContext.setWar("src/test/resources/cprabbs-stub-webapp");
+        cprabbsContext.setContextPath("/cprabbs");
+        server.addHandler(cprabbsContext);
+
+        server.start();
+    }
+
+    public void stopServer() throws Exception {
+        if (server != null && server.isRunning()) {
+            server.stop();
+        }
+        server = null;
+    }
+
+
+    private boolean waitSecondsForPort(int port, long milliseconds) {
+
+        long t0, t1;
+        t0 = System.currentTimeMillis();
+
+        do {
+            t1 = System.currentTimeMillis();
+        }
+        while (t1 - t0 < milliseconds);
+
+        return available(port);
+
+    }
+
+    private static int MIN_PORT_NUMBER = 1025;
+    private static int MAX_PORT_NUMBER = 65535;
+
+    /**
+     * This is borrowed from the Apache MINA project.
+     * Checks to see if a specific port is available.
+     *
+     * @param port the port to check for availability
+     */
+    public static boolean available(int port) {
+        if (port < MIN_PORT_NUMBER || port > MAX_PORT_NUMBER) {
+            throw new IllegalArgumentException("Invalid start port: " + port);
+        }
+
+        ServerSocket ss = null;
+        DatagramSocket ds = null;
+        try {
+            ss = new ServerSocket(port);
+            ss.setReuseAddress(true);
+            ds = new DatagramSocket(port);
+            ds.setReuseAddress(true);
+            return true;
+        } catch (IOException e) {
+        } finally {
+            if (ds != null) {
+                ds.close();
+            }
+
+            if (ss != null) {
+                try {
+                    ss.close();
+                } catch (IOException e) {
+                    /* should not be thrown */
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public int getAvailableLocalPort() throws IOException {
+        int actualPort;
+        ServerSocket serverSocket = new ServerSocket(0);
+        serverSocket.setReuseAddress(true);
+        actualPort = serverSocket.getLocalPort();
+        return actualPort;
+    }
+
+
 }
