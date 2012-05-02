ac94c77c0f5d8da1f320ffd26e4dd485e96aaace
KPN <KPN@KPN-PC.(none)>
2012-05-02 15:03:59 +0200

    More tables and recordspecs

--- common/common/src/main/java/com/trifork/stamdata/specs/SorFullRecordSpecs.java
+++ common/common/src/main/java/com/trifork/stamdata/specs/SorFullRecordSpecs.java
@@ -37,66 +37,47 @@ public class SorFullRecordSpecs {
 	public static final String UPDATED_AT = "updatedAt";
 	
 	public static final RecordSpecification SOR_STATUS = RecordSpecification.createSpecification("SORSorStatus", "PID",
-			field(FROM_DATE, 10),
-			field(TO_DATE,10).doAllowNull(),
-			field(UPDATED_AT,10).doAllowNull(),
-			field(FIRST_FROM_DATE,10)
-		);
-
-	public static final RecordSpecification EAN_LOCATION_CODE_ENTITY = RecordSpecification.createSpecification("SOREanLocationCode", "eanLocationCode",
-			field("eanLocationCode", 20).numerical(),
-			field("onlyInternalIndicator", 1),
-			field("nonActiveIndicator", 1),
-			field("systemSupplier", 20).numerical(),
-			field("systemType", 20).numerical(),
-			field("communicationSupplier", 20).numerical(),
-			field("regionCode", 5).numerical(),
-			field("ediAdministrator", 5).numerical(),
-			field("sorNote", 254).doAllowNull(),
-			field("fkSorStatus", 10).numerical().doAllowNull()
-/*			field(FROM_DATE, 10),
-			field(TO_DATE,10).doAllowNull(),
-			field(UPDATED_AT,10).doAllowNull(),
-			field(FIRST_FROM_DATE,10)*/
-		);
-	
-	public static final RecordSpecification INSTITUTION_OWNER = RecordSpecification.createSpecification("SORInstitutionOwner", "sorIdentifier",
-				field("sorIdentifier", 20).numerical(),
-				field("entityName",60),
-				field("ownerType", 10).numerical(),
-				field(EAN_LOCATION_CODE_ENTITY).doAllowNull(),
-				field("postalAddressInformationId", 10).numerical().doAllowNull(),
-				field("virtualAddressInformationId", 10).numerical().doAllowNull(),
 				field(FROM_DATE, 10),
 				field(TO_DATE,10).doAllowNull(),
 				field(UPDATED_AT,10).doAllowNull(),
 				field(FIRST_FROM_DATE,10)
+		);
+
+	public static final RecordSpecification INSTITUTION_OWNER = RecordSpecification.createSpecification("SORInstitutionOwner", "sorIdentifier",
+				field("sorIdentifier", 20).numerical(),
+				field("entityName",60),
+				field("ownerType", 10).numerical(),
+				field("fkEanLocationCode", 10).doAllowNull(),
+				field("fkPostalAddressInformation", 10).numerical(),
+				field("fkVirtualAddressInfomation", 10).numerical(),
+				field("fkSorStatus", 10).numerical()
 			);
 	
-	
-	public static final RecordSpecification POSTAL_ADDRESS_INFORMATION = RecordSpecification.createSpecification("SORPostalAddressInformation", "PID",
-				field("mailDeliverySublocationIdentifier", 34).doAllowNull(),
-				field("streetName", 40),
-				field("streetNameForAddressingName", 20).doAllowNull(),
-				field("streetBuildingIdentifier", 10),
-				field("floorIdentifier", 10).doAllowNull(),
-				field("suiteIdentifier", 4).doAllowNull(),
-				field("districtSubdivisionIdentifier", 34).doAllowNull(),
-				field("postOfficeBoxIdentifier", 4).numerical().doAllowNull(),
-				field("postCodeIdentifier", 4).numerical(),
-				field("districtName", 34),
-				field("countryIdentificationCodeScheme", 1).numerical().doAllowNull(),
-				field("countryIdentificationCode", 12).doAllowNull(),
-				field("stairway", 40).doAllowNull()
-			);
-	
-	public static final RecordSpecification VIRTUAL_ADDRESS_INFORMATION = RecordSpecification.createSpecification("SORVirtualAddressInformation", "PID",
-				field("emailAddressIdentifier", 254).doAllowNull(),
-				field("website", 254).doAllowNull(),
-				field("telephoneNumberIdentifier", 20),
-				field("faxNumberIdentifier", 20).doAllowNull()
-			);
-	
+    public static final RecordSpecification HEALTH_INSTITUTION_RECORD_TYPE = RecordSpecification.createSpecification("SORHealthInstitution", "SorIdentifier",
+    			// Den første komponent kan blive lige så lang som der er cifre i antallet af enheder:  
+    			//  SOR-kode der relaterer til enheden. Genereres automatisk. En SOR-kode består af fire 
+    			//  komponenter:
+    			//   - Fortløbende nummer 
+    			//   - Namespace (7 cifre: "1000016")
+    			//   - Partition-ID (2 cifre)
+    			//   - Checksum-ciffer (1 cifre).
+    			//  Den første mulige SOR-kode er dermed '11000016002'.
+	            field("sorIdentifier", 20).numerical(),
+	            field("entityName", 60),
+	            field("institutionType", 8).numerical(),
+	            field("pharmacyIdentifier", 20).doAllowNull(),
+	            field("shakIdentifier", 7).doAllowNull(),
+	            field("eanLocationCodeInheritedIndicator", 1).doAllowNull(),
+	            field("fkEanLocationCode", 10).numerical().doAllowNull(),
+	            field("geographicalParentRelation", 10).numerical().doAllowNull(),
+	            field("geographicalParentSorIdentifier", 10).numerical().doAllowNull(),
+	            field("fkPostalAddressInformation", 10).numerical().doAllowNull(),
+	            field("fkVisitingAddressInformation", 10).numerical().doAllowNull(),
+	            field("fkActivityAddressInformation", 10).numerical().doAllowNull(),
+	            field("fkVirtualAddressInfomation", 10).numerical().doAllowNull(),
+	            field("fkSorStatus", 10).numerical()
+    	);
+
 	public static final RecordSpecification ORGANIZATIONAL_UNIT = RecordSpecification.createSpecification("SOROrganizationalUnit", "sorIdentifier",
 				field("sorIdentifier", 20).numerical(),
 				field("entityName", 60),
@@ -126,33 +107,42 @@ public class SorFullRecordSpecs {
 				field("localAttribute3", 20).doAllowNull(),
 				field("localAttribute4", 20).doAllowNull(),
 				field("localAttribute5", 20).doAllowNull()
+		);
+
+	public static final RecordSpecification EAN_LOCATION_CODE_ENTITY = RecordSpecification.createSpecification("SOREanLocationCode", "eanLocationCode",
+				field("eanLocationCode", 20).numerical(),
+				field("onlyInternalIndicator", 1),
+				field("nonActiveIndicator", 1),
+				field("systemSupplier", 20).numerical(),
+				field("systemType", 20).numerical(),
+				field("communicationSupplier", 20).numerical(),
+				field("regionCode", 5).numerical(),
+				field("ediAdministrator", 5).numerical(),
+				field("sorNote", 254).doAllowNull(),
+				field("fkSorStatus", 10).numerical().doAllowNull()
+	);
+    
+	public static final RecordSpecification POSTAL_ADDRESS_INFORMATION = RecordSpecification.createSpecification("SORPostalAddressInformation", "PID",
+				field("mailDeliverySublocationIdentifier", 34).doAllowNull(),
+				field("streetName", 40),
+				field("streetNameForAddressingName", 20).doAllowNull(),
+				field("streetBuildingIdentifier", 10),
+				field("floorIdentifier", 10).doAllowNull(),
+				field("suiteIdentifier", 4).doAllowNull(),
+				field("districtSubdivisionIdentifier", 34).doAllowNull(),
+				field("postOfficeBoxIdentifier", 4).numerical().doAllowNull(),
+				field("postCodeIdentifier", 4).numerical(),
+				field("districtName", 34),
+				field("countryIdentificationCodeScheme", 1).numerical().doAllowNull(),
+				field("countryIdentificationCode", 12).doAllowNull(),
+				field("stairway", 40).doAllowNull()
 			);
 	
-    public static final RecordSpecification HEALTH_INSTITUTION_RECORD_TYPE = RecordSpecification.createSpecification("SORHealthInstitution", "SorIdentifier",
-                field("SorIdentifier", 20).numerical(), //Den første komponent kan blive lige så lang som der er cifre i antallet af enheder:  SOR-kode der relaterer til enheden. Genereres automatisk. En SOR-kode består af fire komponenter: - Fortløbende nummer - Namespace (7 cifre: "1000016") - Partition-ID (2 cifre) - Checksum-ciffer (1 cifre). Den første mulige SOR-kode er dermed '11000016002'.
-                field("EntityName", 60),
-                field("InstitutionType", 8).numerical(),
-                field("PharmacyIdentifier", 20),
-                field("ShakIdentifier", 7)
-                //,field("PostalAddressInformation")
-                //,field("VisitingAddressInformation")
-                //,field("VirtualAddressInformation")
-                ,field("OptionalEanLocationCode", 20).numerical()
+	public static final RecordSpecification VIRTUAL_ADDRESS_INFORMATION = RecordSpecification.createSpecification("SORVirtualAddressInformation", "PID",
+				field("emailAddressIdentifier", 254).doAllowNull(),
+				field("website", 254).doAllowNull(),
+				field("telephoneNumberIdentifier", 20),
+				field("faxNumberIdentifier", 20).doAllowNull()
+			);
 
-                ,field("GeographicalParentRelation", 8).numerical() //Angiver hvilken geografisk tilknytning enheden har. Enten: Selvstændig geografisk placering (1), Den organisatoriske mor (2), Anden geografisk mor (3), Ingen geografisk placering (4), Ukendt geografisk placering (5).
-                ,field("GeographicalParentSorIdentifier", 8).numerical() //Angiver enhedens geografiske mor.
-
-                //,field("SorStatus") //FromDate - Angiver dato for hvornår den sidste ændring gælder fra.,
-                //,field("SorStatus") //ToDate - "Lukke dato" - Angiver den sidste gyldige dato for enheden/lokationsnummeret. Ikke obligatorisk. ,
-                //,field("SorStatus") //UpdatedAt - Angiver dato for hvornår enheden sidst er ændret.
-                //,field("SorStatus") //FirstFromDate - Angiver dato for hvornår enheden er gældende fra.
-
-                //,field("ReplacesEntityCollection") //this is a list of SorIdentifier
-                //,field("ReplacedByEntityCollection") //this is a list of SorIdentifier
-    );
-
-
-
-
-	
 }
--- db/sor_schema.sql
+++ db/sor_schema.sql
@@ -6,12 +6,35 @@ CREATE SCHEMA IF NOT EXISTS `sdm_warehouse` DEFAULT CHARACTER SET utf8 COLLATE u
 USE `sdm_warehouse` ;
 
 -- -----------------------------------------------------
+-- Table `sdm_warehouse`.`SORInstitutionOwner`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `sdm_warehouse`.`SORInstitutionOwner` ;
+
+CREATE  TABLE IF NOT EXISTS `sdm_warehouse`.`SORInstitutionOwner` (
+  `PID` BIGINT NOT NULL AUTO_INCREMENT ,
+  `sorIdentifier` BIGINT NOT NULL ,
+  `entityName` VARCHAR(60) NOT NULL ,
+  `ownerType` BIGINT NOT NULL ,
+  `fkEanLocationCode` BIGINT NULL DEFAULT NULL ,
+  `fkPostalAddressInformation` BIGINT NOT NULL ,
+  `fkVirtualAddressInfomation` BIGINT NOT NULL ,
+  `fkSorStatus` BIGINT NOT NULL ,
+  `ValidFrom` DATETIME NOT NULL ,
+  `ValidTo` DATETIME NULL ,
+  `ModifiedDate` DATETIME NOT NULL ,
+  PRIMARY KEY (`PID`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
+
+
+-- -----------------------------------------------------
 -- Table `sdm_warehouse`.`SOREanLocationCode`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `sdm_warehouse`.`SOREanLocationCode` ;
 
 CREATE  TABLE IF NOT EXISTS `sdm_warehouse`.`SOREanLocationCode` (
-  `pk` BIGINT NOT NULL AUTO_INCREMENT ,
+  `PID` BIGINT NOT NULL AUTO_INCREMENT ,
   `eanLocationCode` BIGINT NOT NULL ,
   `onlyInternalIndicator` CHAR NOT NULL ,
   `nonActiveIndicator` CHAR NOT NULL ,
@@ -21,11 +44,31 @@ CREATE  TABLE IF NOT EXISTS `sdm_warehouse`.`SOREanLocationCode` (
   `regionCode` BIGINT NOT NULL ,
   `ediAdministrator` BIGINT NOT NULL ,
   `sorNote` VARCHAR(254) NULL DEFAULT NULL ,
-  `sorStatusId` BIGINT NULL DEFAULT NULL ,
+  `fkSorStatus` BIGINT NULL DEFAULT NULL ,
   `ValidFrom` DATETIME NOT NULL ,
   `ValidTo` DATETIME NULL ,
   `ModifiedDate` DATETIME NOT NULL ,
-  PRIMARY KEY (`pk`) )
+  PRIMARY KEY (`PID`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
+
+
+-- -----------------------------------------------------
+-- Table `sdm_warehouse`.`SORSorStatus`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `sdm_warehouse`.`SORSorStatus` ;
+
+CREATE  TABLE IF NOT EXISTS `sdm_warehouse`.`SORSorStatus` (
+  `PID` BIGINT NOT NULL AUTO_INCREMENT ,
+  `fromDate` VARCHAR(10) NOT NULL ,
+  `toDate` VARCHAR(10) NULL DEFAULT NULL ,
+  `updatedAt` VARCHAR(10) NULL DEFAULT NULL ,
+  `firstFromDate` VARCHAR(10) NOT NULL ,
+  `ValidFrom` DATETIME NOT NULL ,
+  `ValidTo` DATETIME NULL ,
+  `ModifiedDate` DATETIME NOT NULL ,
+  PRIMARY KEY (`PID`) )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_bin;
@@ -37,7 +80,7 @@ COLLATE = utf8_bin;
 DROP TABLE IF EXISTS `sdm_warehouse`.`SORPostalAddressInformation` ;
 
 CREATE  TABLE IF NOT EXISTS `sdm_warehouse`.`SORPostalAddressInformation` (
-  `pk` BIGINT NOT NULL AUTO_INCREMENT ,
+  `PID` BIGINT NOT NULL AUTO_INCREMENT ,
   `mailDeliverySublocationIdentifier` VARCHAR(34) NULL ,
   `streetName` VARCHAR(40) NOT NULL ,
   `streetNameForAddressingName` VARCHAR(20) NULL ,
@@ -54,7 +97,7 @@ CREATE  TABLE IF NOT EXISTS `sdm_warehouse`.`SORPostalAddressInformation` (
   `ValidFrom` DATETIME NOT NULL ,
   `ValidTo` DATETIME NULL ,
   `ModifiedDate` DATETIME NOT NULL ,
-  PRIMARY KEY (`pk`) )
+  PRIMARY KEY (`PID`) )
 ENGINE = InnoDB;
 
 
@@ -64,7 +107,7 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `sdm_warehouse`.`SORVirtualAddressInformation` ;
 
 CREATE  TABLE IF NOT EXISTS `sdm_warehouse`.`SORVirtualAddressInformation` (
-  `pk` BIGINT NOT NULL AUTO_INCREMENT ,
+  `PID` BIGINT NOT NULL AUTO_INCREMENT ,
   `emailAddressIdentifier` VARCHAR(254) NULL ,
   `website` VARCHAR(254) NULL ,
   `telephoneNumberIdentifier` VARCHAR(20) NOT NULL ,
@@ -72,69 +115,8 @@ CREATE  TABLE IF NOT EXISTS `sdm_warehouse`.`SORVirtualAddressInformation` (
   `ValidFrom` DATETIME NOT NULL ,
   `ValidTo` DATETIME NULL ,
   `ModifiedDate` DATETIME NOT NULL ,
-  PRIMARY KEY (`pk`) )
+  PRIMARY KEY (`PID`) )
 ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `sdm_warehouse`.`SORInstitutionOwner`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `sdm_warehouse`.`SORInstitutionOwner` ;
-
-CREATE  TABLE IF NOT EXISTS `sdm_warehouse`.`SORInstitutionOwner` (
-  `pk` BIGINT NOT NULL AUTO_INCREMENT ,
-  `sorIdentifier` BIGINT NOT NULL ,
-  `entityName` VARCHAR(60) NOT NULL ,
-  `ownerType` BIGINT NOT NULL ,
-  `eanLocationCodeId` BIGINT NULL DEFAULT NULL ,
-  `postalAddressInformationId` BIGINT NULL ,
-  `virtualAddressInformationId` BIGINT NULL ,
-  `sorStatusId` BIGINT NULL ,
-  `ValidFrom` DATETIME NOT NULL ,
-  `ValidTo` DATETIME NULL ,
-  `ModifiedDate` DATETIME NOT NULL ,
-  PRIMARY KEY (`pk`) ,
-  INDEX `fk_eanLocationCode` (`eanLocationCodeId` ASC) ,
-  INDEX `fk_postalAddressInformation` (`postalAddressInformationId` ASC) ,
-  INDEX `fk_virtualAddressInformation` (`virtualAddressInformationId` ASC) ,
-  CONSTRAINT `fk_eanLocationCode`
-    FOREIGN KEY (`eanLocationCodeId` )
-    REFERENCES `sdm_warehouse`.`SOREanLocationCode` (`pk` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_postalAddressInformation`
-    FOREIGN KEY (`postalAddressInformationId` )
-    REFERENCES `sdm_warehouse`.`SORPostalAddressInformation` (`pk` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_virtualAddressInformation`
-    FOREIGN KEY (`virtualAddressInformationId` )
-    REFERENCES `sdm_warehouse`.`SORVirtualAddressInformation` (`pk` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_bin;
-
-
--- -----------------------------------------------------
--- Table `sdm_warehouse`.`SORSorStatus`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `sdm_warehouse`.`SORSorStatus` ;
-
-CREATE  TABLE IF NOT EXISTS `sdm_warehouse`.`SORSorStatus` (
-  `pk` BIGINT NOT NULL AUTO_INCREMENT ,
-  `fromDate` DATE NOT NULL ,
-  `toDate` DATE NULL DEFAULT NULL ,
-  `updatedAt` DATE NULL DEFAULT NULL ,
-  `firstFromDate` DATE NOT NULL ,
-  `ValidFrom` DATETIME NOT NULL ,
-  `ValidTo` DATETIME NULL ,
-  `ModifiedTime` DATETIME NULL ,
-  PRIMARY KEY (`pk`) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_bin;
 
 
 -- -----------------------------------------------------
@@ -143,16 +125,19 @@ COLLATE = utf8_bin;
 DROP TABLE IF EXISTS `sdm_warehouse`.`SOROrganizationalUnit` ;
 
 CREATE  TABLE IF NOT EXISTS `sdm_warehouse`.`SOROrganizationalUnit` (
-  `pk` BIGINT NOT NULL ,
+  `PID` BIGINT NOT NULL AUTO_INCREMENT ,
   `sorIdentifier` BIGINT NOT NULL ,
   `entityName` VARCHAR(60) NOT NULL ,
+  `fkOrganizationalChildUnit` BIGINT NULL ,
   `unitType` BIGINT NULL ,
-  `locationCode` VARCHAR(20) NULL ,
+  `localCode` VARCHAR(20) NULL ,
   `pharmacyIdentifier` VARCHAR(20) NULL ,
   `shakIdentifier` VARCHAR(7) NULL ,
   `providerIdentifier` VARCHAR(9) NULL ,
-  `fkOptionalEanLocationCode` BIGINT NOT NULL ,
-  `fkGeographicalParent` BIGINT NOT NULL ,
+  `eanLocationCodeInheritedIndicator` CHAR(1) NULL ,
+  `fkEanLocationCode` BIGINT NULL ,
+  `geographicalParentRelation` BIGINT NOT NULL ,
+  `geographicalParentSorIdentifier` BIGINT NULL ,
   `fkPostalAddressInformation` BIGINT NULL ,
   `fkVisitingAddressInformation` BIGINT NULL ,
   `fkActivityAddressInformation` BIGINT NULL ,
@@ -172,7 +157,35 @@ CREATE  TABLE IF NOT EXISTS `sdm_warehouse`.`SOROrganizationalUnit` (
   `ValidFrom` DATETIME NOT NULL ,
   `ValidTo` DATETIME NULL ,
   `ModifiedDate` DATETIME NOT NULL ,
-  PRIMARY KEY (`pk`) )
+  PRIMARY KEY (`PID`) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `sdm_warehouse`.`SORHealthInstitution`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `sdm_warehouse`.`SORHealthInstitution` ;
+
+CREATE  TABLE IF NOT EXISTS `sdm_warehouse`.`SORHealthInstitution` (
+  `PID` BIGINT NOT NULL AUTO_INCREMENT ,
+  `sorIdentifier` BIGINT NOT NULL ,
+  `entityName` VARCHAR(60) NOT NULL ,
+  `institutionType` BIGINT NOT NULL ,
+  `pharmacyIdentifier` VARCHAR(20) NULL ,
+  `shakIdentifier` VARCHAR(7) NULL ,
+  `eanLocationCodeInheritedIndicator` VARCHAR(1) NULL ,
+  `fkEanLocationCode` BIGINT NULL ,
+  `geographicalParentRelation` BIGINT NULL ,
+  `geographicalParentSorIdentifier` BIGINT NULL ,
+  `fkPostalAddressInformation` BIGINT NULL ,
+  `fkVisitingAddressInformation` BIGINT NULL ,
+  `fkActivityAddressInformation` BIGINT NULL ,
+  `fkVirtualAddressInfomation` BIGINT NULL ,
+  `fkSorStatus` BIGINT NOT NULL ,
+  `ValidFrom` DATETIME NOT NULL ,
+  `ValidTo` DATETIME NULL ,
+  `ModifiedDate` DATETIME NOT NULL ,
+  PRIMARY KEY (`PID`) )
 ENGINE = InnoDB;
 
 
